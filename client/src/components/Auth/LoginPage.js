283d6a491a9dc8ed1c1308ee65406cc7c67f480a
ArnoldSalcius <arnoldassalcius@gmail.com>
2022-08-17 19:56:09 -0400

    Added Login page styles

--- client/src/components/Auth/LoginPage.js
+++ client/src/components/Auth/LoginPage.js
@@ -1,7 +1,8 @@
 import { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 import { loginUser, clearErrors, loginUserFail } from '../../redux/auth/authActions';
-import { Navigate, useLocation } from 'react-router-dom'
+import { Navigate, useLocation } from 'react-router-dom';
+import './LoginPage.scss';
 
 const initialState = {
     username: '',
@@ -95,64 +96,85 @@ const LoginPage = function ({ register, auth, loginUser, clearAuthErrors, dispat
 
         return (
             <>
-                <label htmlFor='repassword'>Repeat Password</label>
-                <input id='repassword' type='password' name='repassword' value={repassword} onChange={(e) => setRepassword(e.target.value)}></input>
-                <label htmlFor='email'>Email</label>
-                <input style={isEmailError ? { border: '5px solid red' } : null} id='email' name='email' value={input.email} onChange={handleInput}></input>
-                {isEmailError && isEmailError.email}
+                <div className="auth__inputGroup">
+                    <label htmlFor='repassword'>Repeat Password</label>
+                    <input id='repassword' type='password' name='repassword' value={repassword} onChange={(e) => setRepassword(e.target.value)}></input>
+                </div>
+                <div className="auth__inputGroup">
+                    <label htmlFor='email'>Email</label>
+                    <input
+                        style={isEmailError ? { border: '5px solid red' } : null}
+                        id='email'
+                        name='email'
+                        value={input.email}
+                        onChange={handleInput}
+                        placeholder='Anything here...'
+                    />
+                </div>
+                <div className='auth__inputError'>
+                    {isEmailError && isEmailError.email}
+                </div>
             </>
         )
     }
-    // console.log(auth);
-    // if (auth.loading) {
-    //     return null;
-    // }
-    // if (auth.user) {
-    //     console.log('authuser ran');
-    // }
+
 
     return (
         auth.user ? <Navigate to={navigateTo} /> :
-            (<div>
-                <h2>Login</h2>
-                <form>
-                    <label htmlFor='username'>Username</label>
+            (<div className='auth'>
+                <h2 className='auth__header'>{register ? "Register" : "Login"}</h2>
+                <form class='auth__form'>
+                    <div className="auth__inputGroup">
+                        <label htmlFor='username'>Username</label>
 
-                    <input
-                        style={isUsernameError ? { border: '5px red solid' } : null}
-                        autoComplete='off'
-                        id='username'
-                        name='username'
-                        value={input.username}
-                        onChange={handleInput}
-                    >
+                        <input
+                            className={`auth__input${isUsernameError ? '--error' : ''}`}
+                            autoComplete='off'
+                            id='username'
+                            name='username'
+                            placeholder='Username...'
+                            value={input.username}
+                            onChange={handleInput}
+                        />
+                    </div>
+                    <div className='auth__inputError'>
+                        {isUsernameError && isUsernameError.username}
+                    </div>
+                    <div className='auth__inputGroup'>
+                        <label htmlFor='password'>Password</label>
+                        <input
+                            style={isPasswordError ? { border: '5px red solid' } : null}
+                            id='password'
+                            name='password'
+                            placeholder='Password...'
+                            type={showPass ? 'text' : 'password'}
+                            value={input.password}
+                            onChange={handleInput}
+                        >
 
-                    </input>
-                    {isUsernameError && isUsernameError.username}
-                    <label htmlFor='password'>Password</label>
-                    <input
-                        style={isPasswordError ? { border: '5px red solid' } : null}
-                        id='password'
-                        name='password'
-                        type={showPass ? 'text' : 'password'}
-                        value={input.password}
-                        onChange={handleInput}
-                    >
+                        </input>
+                        <div className='auth__inputError'>
+                            {isPasswordError && isPasswordError.password}
 
-                    </input>
-                    {isPasswordError && isPasswordError.password}
-                    <input
-                        type='checkbox'
-                        name='showPass'
-                        id='showPass'
-                        onClick={() => setShowPass(!showPass)}>
-                    </input>
-                    <label htmlFor='showPass' >Show password</label>
+                        </div>
+                        <div>
+                            <input
+                                type='checkbox'
+                                name='showPass'
+                                id='showPass'
+                                onClick={() => setShowPass(!showPass)}>
+                            </input>
+                            <label htmlFor='showPass' >Show password</label>
+                        </div>
+                    </div>
 
                     {register && renderRegister()}
 
                     <button onClick={handleSubmit}>Login</button>
-                    {auth.error && <p style={{ background: 'red' }}>{auth.error.message}</p>}
+                    <div className="auth__error">
+                        {auth.error && <p>{auth.error.message}</p>}
+
+                    </div>
 
                 </form>
 
