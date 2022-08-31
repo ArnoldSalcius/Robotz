dcde65121d10e863e037da491aaf2b56fe01e81a
ArnoldSalcius <arnoldassalcius@gmail.com>
2022-08-30 22:04:32 -0400

    imported path (deployment)

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const logger = require('morgan');
 require('dotenv').config();
+const path = require('path')
 
 const robotRoutes = require('./routes/robot');
 const userRoutes = require('./routes/user');
